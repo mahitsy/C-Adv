d472ca6fbd51b68999483ce7a1bada7d4fda99d8
Mahtisy <david.mahitsy@gmail.com>
2018-02-05 18:45:38 +0200

    3 exercises left

---
+++ Stacks and Queues/Exercise/Stack and Queue/07. Balanced Parenthesis/BalancedParenthesis.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class BalancedParenthesis
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine().ToCharArray();
+        var parenthesesStack = new Stack<char>();
+
+        var isBalanced = true;
+
+        var parentheses = new Dictionary<char, char>
+        {
+            {'{', '}'},
+            { '[', ']'},
+            { '(', ')'}
+        };
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (parentheses.ContainsKey(input[i]))
+            {
+                parenthesesStack.Push(input[i]);
+            }
+            else if (parenthesesStack.Count == 0 || input[i] != parentheses[parenthesesStack.Peek()])
+            {
+                isBalanced = false;
+                break;
+            }
+            else if (input[i] == parentheses[parenthesesStack.Peek()])
+            {
+                parenthesesStack.Pop();
+            }
+        }
+        if (isBalanced) Console.WriteLine("YES");
+
+        else Console.WriteLine("NO");
+    }
+}
+
---
+++ Stacks and Queues/Exercise/Stack and Queue/08. Recursive Fibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+public class RecursiveFibonacci
+{
+    public static void Main()
+    {
+        var wantedFibonacci = int.Parse(Console.ReadLine());
+        long prevNumber = 1;
+        long number = 1;
+        long currentNumber = 0;
+
+        if (wantedFibonacci.Equals(1)|| wantedFibonacci.Equals(2))
+        {
+            Console.WriteLine(1);
+            return;
+        }
+        
+        for (int counter = 2; counter < wantedFibonacci; counter++)
+        {
+            currentNumber = prevNumber + number;
+            prevNumber = number;
+            number = currentNumber;
+            
+
+        }
+
+        Console.WriteLine(currentNumber);
+    }
+}
+
